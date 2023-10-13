2d08ada9890f5c461188047df60007d22361e3fc
nazimnk9 <nazimahmedprovat@gmail.com>
2023-10-13 08:01:04 +0600

    navbar done

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#FFD2A4',
+                nav_backgroud: '#0D0D0D',
+                nav_btn: '1C1D28',
+            },
+            maxWidth: {
+                'container': '1280px',
+            },
+            fontFamily: {
+                robotocondensed: "Roboto Condensed, sans-serif",
+            },
+        }
+    }
+}
